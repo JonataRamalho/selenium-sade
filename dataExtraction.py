88130e661106fa86d4662bcb144e6b5c140a39fa
Jonata Ramalho <jonataramalho.s@gmail.com>
2023-04-08 15:09:20 -0300

    feat: Ajusta data para dia/mês/ano

--- dataExtraction.py
+++ dataExtraction.py
@@ -5,11 +5,13 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pandas as pd
+from datetime import date, datetime
+from dateutil import parser
 
 
 service = Service(executable_path=ChromeDriverManager().install(), port=12345)
 
-url = 'https://al.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios'
+url = 'https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/estado-al?o=21'
 
 driver = webdriver.Chrome(service=service)
 driver.get(url)
@@ -20,25 +22,39 @@ element = driver.find_element(By.XPATH, '//ul[@id="ad-list"]')
 
 ads = element.find_elements(By.XPATH, '//ul[@id="ad-list"]/li')
 
-# TODO: Fazendo
-months = [{
-    "month": 'Jan',
-    "number": 1
-},
-    {
-    "month": 'Fev',
-    "number": 2
-},
-    {
-    "month": 'Mar',
-    "number": 3
-},
-    {
-    "month": 'Abr',
-    "number": 1
-}]
+months = {
+    'jan': 1,
+    'fev': 2,
+    'mar': 3,
+    'abr': 4,
+    'mai': 5,
+    'jun': 6,
+    'jul': 7,
+    'ago': 8,
+    'set': 9,
+    'out': 10,
+    'nov': 11,
+    'dez': 12
+}
 
 dataAds = []
+
+
+def getDate(day):
+    dateToday = date.today()
+
+    if day == 'hoje':
+        return dateToday.strftime('%d/%m/%Y')
+    elif day == 'ontem':
+        yesterday = dateToday - datetime.timedelta(days=1)
+        return yesterday.strftime('%d/%m/%Y')
+    else:
+        abbreviatedDay, abbreviatedMonth = day.split(' de ')
+        month = months[abbreviatedMonth]
+        year = datetime.today().year
+        data = datetime(year, month, int(abbreviatedDay))
+        return data.strftime('%d/%m/%Y')
+
 
 for i in range(len(ads)):
     try:
@@ -57,13 +73,16 @@ for i in range(len(ads)):
         price = element.find_element(
             By.XPATH, '//ul[@id="ad-list"]/li['+str(i+1)+']/a/div[2]/div[1]/div[2]/span')
 
-        publication = element.find_element(
+        day, hour = element.find_element(
             By.XPATH, '//ul[@id="ad-list"]/li['+str(i+1)+']/a/div[2]/div[2]/div[1]/div[2]/span[3]').text.split(',')
 
-        day = publication[0]
+        announcementDate = getDate(day.lower())
 
-        hour = publication[1]
+        print(announcementDate)
 
-        print(hour)
+        # TODO: Falta obter indo de local
+        # TODO: Falta fazer a paginação
+        # TODO: Enviar os dados para tbl_temp_veiculo
+
     except:
         pass
